f52477c84211c43e15540f7b5476bd0fd2ee9c63
carlosperate <carlosperate@users.noreply.github.com>
2018-12-29 15:23:22 +0000

    GUI: Minor refactors to improve code quality

--- ubitflashtool/gui.py
+++ ubitflashtool/gui.py
@@ -41,6 +41,18 @@ class ReadOnlyEditor(Text):
         self.delete = self.redirector.register(
                 'delete', lambda *args, **kw: 'break')
 
+    def clear(self):
+        """Remove all the content in the editor."""
+        self.delete(1.0, 'end')
+
+    def replace(self, new_content):
+        """Remove all editor content and inserts the new content.
+
+        :param new_content: String to insert.
+        """
+        self.delete(1.0, 'end')
+        self.insert(1.0, new_content)
+
 
 class StdoutRedirector(object):
     """A class to redirect stdout to a text widget."""
@@ -49,7 +61,7 @@ class StdoutRedirector(object):
         """Get the text area widget as a reference and configure its colour."""
         self.text_area = text_area
         self.text_color = text_color
-        self.tag = 'colour_change_%s' % text_color
+        self.tag = 'colour_change_{}'.format(text_color)
         if self.text_color:
             self.text_area.tag_configure(self.tag, foreground=text_color)
 
@@ -75,33 +87,20 @@ class TextViewer(ReadOnlyEditor):
         :param frame: A Frame() instance to set the text editor.
         """
         self.scrollbar = Scrollbar(parent, orient='vertical')
-        ReadOnlyEditor.__init__(self, parent,
-                                yscrollcommand=self.scrollbar.set,
-                                *args, **kwargs)
+        ReadOnlyEditor.__init__(
+            self, parent, yscrollcommand=self.scrollbar.set, *args, **kwargs)
         self.pack(side='left', fill='both', expand=1)
         self.config(wrap='char', width=1)
         self.scrollbar.pack(side='right', fill='y')
         self.scrollbar.config(command=self.yview)
         parent.pack(fill='both', expand=1)
 
-    def clear(self):
-        """Remove all the content in the editor."""
-        self.delete(1.0, 'end')
-
-    def replace(self, new_content):
-        """Remove all editor content and inserts the new content.
-
-        :param new_content: String to insert.
-        """
-        self.delete(1.0, 'end')
-        self.insert(1.0, new_content)
-
 
 class ConsoleOutput(ReadOnlyEditor):
     """A read-only editor to display std out and err streams."""
 
     def __init__(self, parent, *args, **kwargs):
-        """Construct the editor into a parent frame.
+        """Construct the read-only editor into a parent frame.
 
         :param frame: A Frame() instance to set this text editor.
         """
@@ -138,7 +137,8 @@ class ConsoleOutput(ReadOnlyEditor):
 class UBitFlashToolWindow(Tk):
     """Main app window.
 
-    Attaches a Frame to a TK window with a text editor and menus for options.
+    Creates a TK window with a text viewer, console viewer, and menus for
+    executing actions.
     """
 
     def __init__(self, *args, **kwargs):
@@ -149,18 +149,17 @@ class UBitFlashToolWindow(Tk):
 
         self.menu_bar = Menu(self)
         self.set_menu_bar(self.menu_bar)
+        self.bind_shortcuts()
 
-        self.frame_editor = Frame(self)
-        self.editor = TextViewer(self.frame_editor)
-        self.editor.focus()
+        self.frame_viewer = Frame(self)
+        self.text_viewer = TextViewer(self.frame_viewer)
+        self.text_viewer.focus()
 
         self.frame_console = Frame(self)
         self.console = ConsoleOutput(self.frame_console)
 
         # instead of closing the window, execute a function
-        self.protocol('WM_DELETE_WINDOW', self.file_quit)
-
-        self.bind_shortcuts()
+        self.protocol('WM_DELETE_WINDOW', self.app_quit)
 
     def set_menu_bar(self, menu):
         """Create the menu bar with all user options.
@@ -179,7 +178,7 @@ class UBitFlashToolWindow(Tk):
                               accelerator='{}+S'.format(cmd_key))
         file_menu.add_separator()
         file_menu.add_command(label='Exit',
-                              command=self.file_quit, accelerator='Alt+F4')
+                              command=self.app_quit, accelerator='Alt+F4')
         menu.add_cascade(label='File', underline=0, menu=file_menu)
         # Menu item micro:bit
         ubit_menu = Menu(menu, tearoff=0)
@@ -222,43 +221,43 @@ class UBitFlashToolWindow(Tk):
     def read_python_code(self):
         """Read the Python user code from the micro:bit flash.
 
-        Displays it as text code in the read-only text editor.
+        Displays it as text code in the read-only text viewer.
         """
         python_code = read_python_code()
-        self.editor.replace(python_code)
+        self.text_viewer.replace(python_code)
 
     def read_micropython(self):
         """Read the Python user code from the micro:bit flash.
 
-        Displays it as Intel Hex in the read-only text editor.
+        Displays it as Intel Hex in the read-only text viewer.
         """
         hex_str = read_micropython()
-        self.editor.replace(hex_str)
+        self.text_viewer.replace(hex_str)
 
     def read_full_flash_intel(self):
         """Read the full contents of flash.
 
-        Displays it as Intel Hex in the read-only text editor.
+        Displays it as Intel Hex in the read-only text viewer.
         """
         hex_str = read_full_flash_hex(decode_hex=False)
-        self.editor.replace(hex_str)
+        self.text_viewer.replace(hex_str)
 
     def read_full_flash_pretty(self):
         """Read the full contents of flash.
 
         Displays it as a pretty hex and ASCII string in the read-only text
-        editor.
+        viewer.
         """
         hex_str = read_full_flash_hex(decode_hex=True)
-        self.editor.replace(hex_str)
+        self.text_viewer.replace(hex_str)
 
     def read_uicr_customer(self):
         """Read the full contents of flash.
 
-        Displays it as Intel Hex in the read-only text editor.
+        Displays it as Intel Hex in the read-only text viewer.
         """
         uicr_hex_str = read_uicr_customer(decode_hex=True)
-        self.editor.replace(uicr_hex_str)
+        self.text_viewer.replace(uicr_hex_str)
 
     def compare_full_flash_intel(self):
         """Compare a hex file with the micro:bit flash.
@@ -268,9 +267,9 @@ class UBitFlashToolWindow(Tk):
         """
         file_path = filedialog.askopenfilename()
         if file_path:
-            self.editor.replace('Reading flash contents...')
+            self.text_viewer.replace('Reading flash contents...')
             compare_full_flash_hex(file_path)
-            self.editor.replace('Diff content loaded in default browser.')
+            self.text_viewer.replace('Diff content loaded in default browser.')
 
     def compare_uicr_customer_intel(self):
         """Compare a hex file with the micro:bit user UICR memory.
@@ -281,36 +280,35 @@ class UBitFlashToolWindow(Tk):
         """
         file_path = filedialog.askopenfilename()
         if file_path:
-            self.editor.replace('Reading User UICR contents...')
+            self.text_viewer.replace('Reading User UICR contents...')
             compare_uicr_customer(file_path)
-            self.editor.replace('Diff content loaded in default browser.')
+            self.text_viewer.replace('Diff content loaded in default browser.')
 
     def file_open(self, event=None):
-        """Open a file picker and loads a file into the editor."""
+        """Open a file picker and load a file into the text viewer."""
         file_path = filedialog.askopenfilename()
         if file_path:
             with open(file_path, encoding='utf-8') as f:
                 file_contents = f.read()
             # Set current text to file contents
-            self.editor.replace(file_contents)
+            self.text_viewer.replace(file_contents)
 
     def file_save_as(self, event=None):
-        """Save the text in the text editor into a file."""
+        """Save the text from the text viewer into a file."""
         file_path = filedialog.asksaveasfilename(filetypes=(
                 ('Python files', '*.py *.pyw'), ('All files', '*.*')))
         if file_path:
             with open(file_path, 'wb') as f:
-                text = self.editor.get(1.0, 'end-1c')
+                text = self.text_viewer.get(1.0, 'end-1c')
                 f.write(text.encode('utf-8'))
                 return file_path
         else:
             return None
 
-    def file_quit(self, event=None):
-        """Confirm with the user to quite the app."""
-        if messagebox.askyesnocancel('Exit', 'Exit?'):
-            self.console.deactivate()
-            self.destroy()
+    def app_quit(self, event=None):
+        """Quit the app."""
+        self.console.deactivate()
+        self.destroy()
 
 
 def open_gui():
