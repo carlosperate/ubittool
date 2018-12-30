ace25525e67d8f7703e7dbe694e4eda6fce740a2
carlosperate <carlosperate@users.noreply.github.com>
2018-12-30 20:06:37 +0000

    Tests: Add cmd._bytes_to_intel_hex tests

--- tests/test_cmds.py
+++ tests/test_cmds.py
@@ -9,9 +9,61 @@ except ImportError:
 
 from ubitflashtool import cmds
 
+INTEL_HEX_EOF = ':00000001FF\n'
 
-@mock.patch('ubitflashtool.cmds.Timer')
-@mock.patch('ubitflashtool.cmds.webbrowser.open')
+
+def test_bytes_to_intel_hex():
+    """Test the data to Intel Hex string conversion."""
+    data = [1, 2, 3, 4, 5]
+    expected_hex_str = '\n'.join([':050000000102030405EC', INTEL_HEX_EOF])
+
+    result = cmds._bytes_to_intel_hex(data=data)
+
+    assert expected_hex_str == result
+
+
+def test_bytes_to_intel_hex_offset():
+    """Test the data to Intel Hex string conversion."""
+    data = [1, 2, 3, 4, 5]
+    offset = 0x2000000
+    expected_hex_str = '\n'.join([':020000040200F8',
+                                  ':050000000102030405EC',
+                                  INTEL_HEX_EOF])
+
+    result = cmds._bytes_to_intel_hex(data=data, offset=offset)
+
+    assert expected_hex_str == result
+
+
+@mock.patch('ubitflashtool.cmds.sys.stderr.write', autospec=True)
+@mock.patch('ubitflashtool.cmds.StringIO', autospec=True)
+def test_bytes_to_intel_hex_io_error(mock_string_io, mock_stderr):
+    """Test the exception handling when an IOError is encountered."""
+    data = [1, 2, 3, 4, 5]
+    mock_string_io.return_value.write.side_effect = IOError()
+
+    result = cmds._bytes_to_intel_hex(data=data)
+
+    assert result is None
+    assert mock_stderr.call_count == 1
+
+
+def test_bytes_to_intel_hex_invalid_data():
+    """Test the data to Intel Hex string conversion."""
+    data = [1, 2, 3, 4, '500']
+
+    try:
+        cmds._bytes_to_intel_hex(data=data)
+    except Exception:
+        # The exception that bubbles up from IntelHex is implementation detail
+        # from that library, so it could be anything
+        assert True, 'Exception raised'
+    else:
+        assert False, 'Exception NOT raised'
+
+
+@mock.patch('ubitflashtool.cmds.Timer', autospec=True)
+@mock.patch('ubitflashtool.cmds.webbrowser.open', autospec=True)
 def test_open_temp_html(mock_browser_open, mock_timer):
     """Check browser is requested with a file containing the given text."""
     html_content = 'hello world'
@@ -31,7 +83,7 @@ def test_open_temp_html(mock_browser_open, mock_timer):
     os.remove(file_path)
 
 
-@mock.patch('ubitflashtool.cmds.HtmlDiff')
+@mock.patch('ubitflashtool.cmds.HtmlDiff', autospec=True)
 def test_gen_diff_html(mock_diff):
     """Check the HTML returned contains the provided inputs."""
     from_title = 'from_title_content'
@@ -49,9 +101,9 @@ def test_gen_diff_html(mock_diff):
     assert html.count(to_lines) == 1
 
 
-@mock.patch('ubitflashtool.cmds.read_full_flash_hex')
-@mock.patch('ubitflashtool.cmds._gen_diff_html')
-@mock.patch('ubitflashtool.cmds._open_temp_html')
+@mock.patch('ubitflashtool.cmds.read_full_flash_hex', autospec=True)
+@mock.patch('ubitflashtool.cmds._gen_diff_html', autospec=True)
+@mock.patch('ubitflashtool.cmds._open_temp_html', autospec=True)
 def test_compare_full_flash_hex(
         mock_open_temp_html, mock_gen_diff_html, mock_read_full_flash_hex):
     """Check that file contents."""
@@ -73,9 +125,9 @@ def test_compare_full_flash_hex(
     assert mock_open_temp_html.call_count == 1
 
 
-@mock.patch('ubitflashtool.cmds.read_uicr_customer')
-@mock.patch('ubitflashtool.cmds._gen_diff_html')
-@mock.patch('ubitflashtool.cmds._open_temp_html')
+@mock.patch('ubitflashtool.cmds.read_uicr_customer', autospec=True)
+@mock.patch('ubitflashtool.cmds._gen_diff_html', autospec=True)
+@mock.patch('ubitflashtool.cmds._open_temp_html', autospec=True)
 def test_compare_uicr_customer(
         mock_open_temp_html, mock_gen_diff_html, mock_read_uicr_customer):
     """Check that file contents."""
