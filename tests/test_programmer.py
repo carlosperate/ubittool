b1a16bbb7f50a20d0dbed7f65a7e53d012f7d62d
carlosperate <carlosperate@users.noreply.github.com>
2022-01-03 00:04:07 +0000

    Tests: Add programmer MicrobitMcu.read_uicr() & uicr_customer() tests.

--- tests/test_programmer.py
+++ tests/test_programmer.py
@@ -225,3 +225,122 @@ def test_read_ram_bad_count(mock_read_memory):
     assert "Cannot read a RAM location out of" in str(execinfo12.value)
     assert "Cannot read a RAM location out of" in str(execinfo21.value)
     assert "Cannot read a RAM location out of" in str(execinfo22.value)
+
+
+###############################################################################
+# MicrobitMcu.read_uicr()
+###############################################################################
+@mock.patch.object(programmer.MicrobitMcu, "_read_memory", autospec=True)
+def test_read_uicr(mock_read_memory):
+    """Test read_uicr() with default arguments."""
+    data_bytes = bytes([x for x in range(256)] * 4)
+    mock_read_memory.return_value = data_bytes
+    mb1 = MicrobitMcu_instance(v=1)
+    mb2 = MicrobitMcu_instance(v=2)
+
+    start_addres1, result_data1 = mb1.read_uicr()
+    start_addres2, result_data2 = mb2.read_uicr()
+
+    assert start_addres1 == 0x1000_1000
+    assert result_data1 == data_bytes
+    assert start_addres2 == 0x1000_1000
+    assert result_data2 == data_bytes
+
+
+@mock.patch.object(programmer.MicrobitMcu, "_read_memory", autospec=True)
+def test_read_uicr_with_args(mock_read_memory):
+    """Test read_uicr() with providedarguments."""
+    data_bytes = bytes([x for x in range(256)] * 4)
+    mock_read_memory.return_value = data_bytes
+    mb1 = MicrobitMcu_instance(v=1)
+    mb2 = MicrobitMcu_instance(v=2)
+
+    start_addres1, result_data1 = mb1.read_uicr(
+        address=0x1000_1000, count=0x100
+    )
+    start_addres2, result_data2 = mb2.read_uicr(
+        address=0x1000_1000, count=0x308
+    )
+
+    assert start_addres1 == 0x1000_1000
+    assert result_data1 == data_bytes
+    assert start_addres2 == 0x1000_1000
+    assert result_data2 == data_bytes
+
+
+@mock.patch.object(programmer.MicrobitMcu, "_read_memory", autospec=True)
+def test_read_uicr_bad_address(mock_read_memory):
+    """Test read_uicr() with bad address arguments."""
+    data_bytes = bytes([x for x in range(256)] * 4)
+    mock_read_memory.return_value = data_bytes
+    mb1 = MicrobitMcu_instance(v=1)
+    mb2 = MicrobitMcu_instance(v=2)
+
+    with pytest.raises(ValueError) as execinfo11:
+        start_addres, result_data = mb1.read_uicr(
+            address=0x1000_1000 - 1, count=1
+        )
+    with pytest.raises(ValueError) as execinfo12:
+        start_addres, result_data = mb1.read_uicr(address=0x100 + 1, count=1)
+    with pytest.raises(ValueError) as execinfo21:
+        start_addres, result_data = mb2.read_uicr(
+            address=0x1000_1000 - 1, count=1
+        )
+    with pytest.raises(ValueError) as execinfo22:
+        start_addres, result_data = mb2.read_uicr(address=0x308 + 1, count=1)
+
+    assert "Cannot read a UICR location out of" in str(execinfo11.value)
+    assert "Cannot read a UICR location out of" in str(execinfo12.value)
+    assert "Cannot read a UICR location out of" in str(execinfo21.value)
+    assert "Cannot read a UICR location out of" in str(execinfo22.value)
+
+
+@mock.patch.object(programmer.MicrobitMcu, "_read_memory", autospec=True)
+def test_read_uicr_bad_count(mock_read_memory):
+    """Test read_uicr() with bad values in the count argument."""
+    data_bytes = bytes([x for x in range(256)] * 4)
+    mock_read_memory.return_value = data_bytes
+    mb1 = MicrobitMcu_instance(v=1)
+    mb2 = MicrobitMcu_instance(v=2)
+
+    with pytest.raises(ValueError) as execinfo11:
+        start_addres, result_data = mb1.read_uicr(
+            address=0x1000_1000, count=(16 * 1024) + 1
+        )
+    with pytest.raises(ValueError) as execinfo12:
+        start_addres, result_data = mb1.read_uicr(
+            address=0x1000_1000 + (16 * 1024) - 10, count=11
+        )
+    with pytest.raises(ValueError) as execinfo21:
+        start_addres, result_data = mb2.read_uicr(
+            address=0x1000_1000, count=(128 * 1024) + 1
+        )
+    with pytest.raises(ValueError) as execinfo22:
+        start_addres, result_data = mb2.read_uicr(
+            address=0x1000_1000 + (128 * 1024) - 10, count=11
+        )
+
+    assert "Cannot read a UICR location out of" in str(execinfo11.value)
+    assert "Cannot read a UICR location out of" in str(execinfo12.value)
+    assert "Cannot read a UICR location out of" in str(execinfo21.value)
+    assert "Cannot read a UICR location out of" in str(execinfo22.value)
+
+
+###############################################################################
+# MicrobitMcu.read_uicr_customer()
+###############################################################################
+@mock.patch.object(programmer.MicrobitMcu, "_read_memory", autospec=True)
+def test_read_uicr_customer(mock_read_memory):
+    """Test read_uiread_uicr_customercr() with default arguments."""
+    data_bytes = bytes([x for x in range(256)] * 4)
+    mock_read_memory.return_value = data_bytes
+    mb1 = MicrobitMcu_instance(v=1)
+    mb2 = MicrobitMcu_instance(v=2)
+
+    start_addres1, result_data1 = mb1.read_uicr_customer()
+    start_addres2, result_data2 = mb2.read_uicr_customer()
+
+    assert start_addres1 == 0x1000_1080
+    assert result_data1 == data_bytes
+    assert start_addres2 == 0x1000_1080
+    assert result_data2 == data_bytes
